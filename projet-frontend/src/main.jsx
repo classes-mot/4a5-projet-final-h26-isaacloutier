861853dac4a0a9efd15b7e5b77ac15919c382b7d
Isaac Cloutier <2496147@cmontmorency.qc.ca>
2026-05-06 14:23:12 -0400

    FAIT: Correction de la variable d'environnement

--- projet-frontend/src/main.jsx
+++ projet-frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import "../src/traduction/i18n/i18n.jsx";
 //mongodb+srv://2496147:28S3pt3mbr32007!@clusterprojetfinal.w3ka0kp.mongodb.net/?appName=ClusterProjetFinal
+//https://foura5-projet-final-h26-isaacloutier-4.onrender.com
+//https://woofontrency.onrender.com
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
